b4a6e6ec56338593a1a7b1f6c92d17da3ddbc0a4
spencerckhuang <shuan126@jh.edu>
2024-11-13 13:47:02 -0500

    yojson test

---
+++ OCamaps/demo/yojson/yojson_test.ml
@@ -0,0 +1,10 @@
+let json_string = {|
+  {"number" : 42,
+   "string" : "yes",
+   "list": ["for", "sure", 42]}|}
+(* val json_string : string *)
+
+let json = Yojson.Safe.from_string json_string
+(* val json : Yojson.Safe.t *)
+
+let () = Format.printf "Parsed to %a" Yojson.Safe.pp json
