f80d77820694bd6fa4ea2c230a39392b282dac85
lcai18 <lcai18@jh.edu>
2024-11-21 14:59:17 -0500

    split interface

---
+++ lib/src/path_finding/path_find.mli
@@ -0,0 +1,4 @@
+open Utils
+
+(** [get_shortest_path m loc1 loc2] returns a [route] representing the shortest path between [loc1] and [loc2] on map [m]. If no path exists, returns [None] *)
+val get_shortest_path : map -> location -> location -> route option
