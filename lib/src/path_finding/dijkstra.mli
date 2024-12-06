7be67384e8a909208a734b9d92e311af4fee8446
lcai18 <lcai18@jh.edu>
2024-12-06 17:05:23 -0500

    removing redundant comment

--- lib/src/path_finding/dijkstra.mli
+++ lib/src/path_finding/dijkstra.mli
@@ -1,8 +1,6 @@
 open Types
 open Graph 
-(* Given a graph, a start location, and a destination location,
-   return the shortest distance between them using Dijkstra's algorithm 
-val shortest_distance : graph -> start:location -> dest:location -> float option*)
+
 
 (* Given a graph, a start location, and a destination location,
    return the shortest path (sequence of locations) between them and the total cost.
