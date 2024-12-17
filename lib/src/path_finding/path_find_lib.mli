2fd7b442ab263b1cefab52dee813e11280c52b05
spencerckhuang <shuan126@jh.edu>
2024-12-16 21:03:00 -0500

    slight .mli file rewrite

--- lib/src/path_finding/path_find_lib.mli
+++ lib/src/path_finding/path_find_lib.mli
@@ -8,40 +8,20 @@ module StringMap : sig
 (** Type alias for a map of node labels. *)
 type node_label_map = string StringMap.t
 
-(** Load a node label map from a file. 
-
-    @param filepath The path to the file containing the node label map in S-expression format.
-    @return A map from node labels to node IDs.
-*)
+(** Load a node label map from [filepath]. Returns a map which maps custom node labels to their Overpass IDs. *)
 val load_node_label_map : filepath:string -> node_label_map
 
-(** Convert a location label to its corresponding node ID.
-
-    @param label The location label to convert.
-    @return An option containing the node ID, or [None] if not found.
-*)
+(** Convert a location [label] to its corresponding node ID. *)
 val location_label_to_id : string -> string option
 
-(** Convert a location label to a node ID, returning the label itself if no mapping exists.
-
-    @param label The location label to convert.
-    @return The corresponding node ID if found, otherwise the original label.
-*)
+(** Convert a location [label] to a node ID, returning the label itself if no such mapping is found. *)
 val location_label_to_id_if_specified : string -> string
 
-(** Retrieve a graph and ID map, either from a file or by creating a new graph.
-
-    @param graph_path The optional path to a file containing a serialized graph.
-    @return An option containing the graph and ID map, or [None] if the graph could not be created.
-*)
+(** Retrieve a graph and ID map, either from a [graph_path] or by creating a new graph. *)
 val get_graph : string option -> (Graph.graph * Graph.id_map) option
 
-(** Find and print the shortest path between two locations.
-
-    @param loc_name_1 The name or ID of the starting location.
-    @param loc_name_2 The name or ID of the destination location.
-    @param graph_path The optional path to a file containing a serialized graph.
-*)
+(** Find and print the shortest path between two locations: [loc_name_1] and [loc_name_2]. Returns a [graph_path], a list of locations which compose the optimal path,
+    and a total [distance] of the path. *)
 val find_shortest_path_lib : string -> string -> string option -> (location list * float) option
 
 val shortest_path_to_json : location list * float -> Yojson.Basic.t
