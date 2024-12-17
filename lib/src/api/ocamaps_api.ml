c11bcfd04a2fbd284e37ee55d3eccd5bb888cc2f
spencerckhuang <shuan126@jh.edu>
2024-12-17 00:56:37 -0500

    removed unnecessary commented code

--- lib/src/api/ocamaps_api.ml
+++ lib/src/api/ocamaps_api.ml
@@ -1,29 +1,5 @@
 open Utils
 
-(* let () = 
-  Dream.run ~port:5432 
-  @@ Dream.logger
-  @@ Dream.set_header "Access-Control-Allow-Origin" "*";  (* Allow any origin *)
-  @@ Dream.set_header "Access-Control-Allow-Methods" "GET, POST, OPTIONS"; (* Allow specific methods *)
-  @@ Dream.set_header "Access-Control-Allow-Headers" "Content-Type";
-  @@ Dream.middleware (fun request -> 
-        
-        request
-      )
-  @@ Dream.router [
-    
-    Dream.get "/"
-      (fun _ -> Dream.html "hello world!");
-
-    Dream.get "/directions/:start/:destination"
-      (fun request -> 
-        let api_get_res = get_path (Dream.param request "start") (Dream.param request "destination") in
-        Dream.json (Yojson.Basic.to_string api_get_res)
-      )
-  ]
-   *)
-
-
 let () =
   Dream.run ~port:5432
   @@ Dream.logger
